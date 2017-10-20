c653272a9906c838ff16ddbbb24ea56b9801a1a0
Unknown <unknown@example.com>
2017-10-20 14:40:15 +0200

    First Push
    Adding all math files.

---
+++ Trident/Engine/Math.cpp
@@ -0,0 +1,12 @@
+#include <Math.h>
+#include <cmath>
+
+const float Math::PI = 3.1415926535f;
+float Math::Clamp(float x, float i, float j) { return x > j ? j : (x < i ? i : x); }
+float Math::Mod(float x, float y) { return Fract(x / y) * y; }
+float Math::Fract(float x) { return x - Floor(x); }
+float Math::Min(float x, float y) { return x < y ? x : y; }
+float Math::Max(float x, float y) { return x > y ? x : y; }
+float Math::Floor(float x) { return floor(x); }
+float Math::Sqrt(float x) { return sqrt(x); }
+float Math::Ceil(float x) { return floor(x) == x ? x : floor(x) + 1 * (x < 0 ? -1 : 1); }
---
+++ Trident/Engine/Math.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class Math {
+
+public:
+	static const float PI;
+
+	//! Fast absolute calculation for floats.
+	inline static float fabsf(float f) { return (f < 0) ? -f : f; }
+	//! Fast absolute calculation for ints.
+	inline static int fabsi(int i) { return (i < 0) ? -i : i; }
+
+	static float Clamp(float x, float i, float j);
+	static float Mod(float x, float y);
+	static float Floor(float x);
+	static float Fract(float x);
+	static float Min(float x, float y);
+	static float Max(float x, float y);
+	static float Sqrt(float x);
+	static float Ceil(float x);
+
+};
